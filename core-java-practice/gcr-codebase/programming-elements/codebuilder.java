81a30fb26376263fc8d53f8586efb7ed813f7b33
Devesh Varshney <deveshvarshney64@gmail.com>
2026-06-08 13:42:52 +0530

    Added core java practice code

---
+++ core-java-practice/gcr-codebase/programming-elements/codebuilder.java
@@ -0,0 +1,19 @@
+public class codebuilder {
+    public static void main(String[] args) {
+       String name = "Ravi";
+       int age = 19;
+       int rank = 2;
+       int salary = 50000;
+       float MembershipFee = 899.9f;
+       double bonus = salary*0.12;
+       int bonusInt = (int)bonus;
+        System.out.println("***WELCOME CARD***");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Rank: " + rank);
+        System.out.println("Salary: ₹" + salary);
+        System.out.println("Membership Fee: ₹" + MembershipFee);
+        System.out.println("Annual Bonus (12%): ₹" + bonusInt);
+    }
+}
+
