0f2bb37f7b744f95658ceb14bbc040d6db26a456
Devesh Varshney <deveshvarshney64@gmail.com>
2026-07-23 11:23:01 +0530

    Added GRAPHS Practice Questions

---
+++ Graph/DFSAllPaths.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class DFSAllPaths {
+    void allRoutes(Map<Integer, List<Integer>> roads, int current, int target,
+                   List<Integer> path, Set<Integer> visited, List<List<Integer>> allRoutes) {
+        path.add(current);
+        visited.add(current);
+        if (current == target) {
+            allRoutes.add(new ArrayList<>(path));
+        } else {
+            for (int next : roads.getOrDefault(current, Collections.emptyList())) {
+                if (!visited.contains(next)) {
+                    allRoutes(roads, next, target, path, visited, allRoutes);
+                }
+            }
+        }
+        path.remove(path.size() - 1); // backtrack
+        visited.remove(current); // backtrack
+    }
+}
